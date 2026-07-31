c0ab0d208296425fc519363bc794d0695766ef13
Pranay Shreshth <152575263+shpranay@users.noreply.github.com>
2026-07-31 19:44:42 +0530

    Add: python-sort-ascending - submission-2

---
+++ Python Coding Interviews/python-sort-ascending/submission-2.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def sort_words(words: List[str]) -> List[str]:
+    words.sort()
+    return words
+
+def sort_numbers(numbers: List[int]) -> List[int]:
+    numbers.sort()
+    return numbers
+
+def sort_decimals(numbers: List[float]) -> List[float]:
+    numbers.sort()
+    return numbers
