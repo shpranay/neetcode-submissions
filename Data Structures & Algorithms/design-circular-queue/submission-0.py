d75f13c0acde8717eb5caf8b96027dbfd4672467
Pranay Shreshth <152575263+shpranay@users.noreply.github.com>
2026-08-31 20:34:01 +0530

    Add: design-circular-queue - submission-0

---
+++ Data Structures & Algorithms/design-circular-queue/submission-0.py
@@ -0,0 +1,45 @@
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.queue = [0] * k
+        self.k = k
+        self.front = 0
+        self.rear = 0
+        self.size = 0
+
+    def enQueue(self, value: int) -> bool:
+        if self.isFull():
+            return False
+
+        self.queue[self.rear] = value
+        self.rear = (self.rear + 1) % self.k
+        self.size += 1
+
+        return True
+
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+
+        self.front = (self.front + 1) % self.k
+        self.size -= 1
+
+        return True
+
+    def Front(self) -> int:
+        if self.isEmpty():
+            return -1
+
+        return self.queue[self.front]
+
+    def Rear(self) -> int:
+        if self.isEmpty():
+            return -1
+
+        return self.queue[(self.rear - 1) % self.k]
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        return self.size == self.k
